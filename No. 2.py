4c34a53553f69c8be39dd774d981535ce69d7f25
Dilla safira <72483342+Dilla444@users.noreply.github.com>
2022-04-07 19:21:28 +0700

    Update No. 2.py

--- No. 2.py
+++ No. 2.py
@@ -5,33 +5,109 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
-import sys
-
-
-def draw():
-    glClear(GL_COLOR_BUFFER_BIT)
-
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    glOrtho(-6, 6, -6, 6, -1, 1)
-
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-
-    glColor3f(1, 1, 1)
-    glBegin(GL_LINE_STRIP)
-    glVertex2f(0.00, 4.00)
-    glVertex2f(4.00, 4.00)
-    glVertex2f(4.00, 0.00)
-    glVertex2f(0.0, 0.00)
-    glVertex2f(0.00, 4.00)
+ 
+ 
+def init():
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    gluOrtho2D(-100.0, 100.0, -100.0, 100.0)
+    glPointSize(5)
+ 
+ 
+def plot(x, y):
+    glBegin(GL_POINTS)
+    glVertex2f(x, y)
     glEnd()
-
-    glutSwapBuffers()
-
-
-glutInit(sys.argv)
-glutInitWindowSize(500, 500)
-glutCreateWindow("UTS GRAFIKA KOMPUTER")
-glutDisplayFunc(draw)
-glutMainLoop()
+ 
+ 
+def bresenham_circle_drawing(r):
+ 
+    # lokasi lingkaran yang akan dibuat
+ 
+    x_position = -50
+    y_position = 50
+ 
+    x = 0
+    y = r
+ 
+    # decision parameter
+    d = 3 - 2 * r
+ 
+    # membuat sebuah titik pada koordinat yang ditentukan
+    plot(x + x_position, y + y_position)
+ 
+    while y > x:
+ 
+        if d < 0:
+            x += 1
+            d += 4 * x + 6
+        else:
+            x += 1
+            y -= 1
+            d += (4 * (x - y)) + 10
+ 
+        # Tidak perlu mencari semua nilai koordinat
+        # cukup dapatkan nilai (x, y) saja
+        # lalu balik menjadi (y, x)
+ 
+        # Untuk pixel (x, y)
+ 
+        # Quadrant 1
+        plot(x + x_position, y + y_position)
+ 
+        # Quadrant 2
+        plot(x + x_position, -y + y_position)
+ 
+        # Quadrant 3
+        plot(-x + x_position, -y + y_position)
+ 
+        # Quadrant 4
+        plot(-x + x_position, y + y_position)
+ 
+        # Untuk pixel (y, x)
+ 
+        # Quadrant 1
+        plot(y + x_position, x + y_position)
+ 
+        # Quadrant 2
+        plot(-y + x_position, x + y_position)
+ 
+        # Quadrant 3
+        plot(-y + x_position, -x + y_position)
+ 
+        # Quadrant 4
+        plot(y + x_position, -x + y_position)
+ 
+ 
+def plotpoints():
+ 
+    glClear(GL_COLOR_BUFFER_BIT)
+    glColor3f(1, 1.0, 1.0)
+ 
+    glBegin(GL_LINES)
+ 
+    glVertex2f(-100, 0)
+    glVertex2f(100, 0)
+ 
+    glVertex2f(0, -100)
+    glVertex2f(0, 100)
+ 
+    glEnd()
+ 
+    bresenham_circle_drawing(40)
+ 
+    glFlush()
+ 
+ 
+def main():
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+    glutInitWindowSize(500, 500)
+    glutInitWindowPosition(100, 100)
+    glutCreateWindow("Bresenham Circle Drawing Algoritm")
+    glutDisplayFunc(plotpoints)
+ 
+    init()
+    glutMainLoop()
+ 
+ 
+main()
